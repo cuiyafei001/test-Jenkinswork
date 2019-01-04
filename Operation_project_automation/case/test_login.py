54d14c649dccb2e15bdfd946845f4a1a121b926c
cuiyafei001@163.com <cuiyafei001@163.com>
2019-01-04 13:55:48 +0800

    testwork

--- Operation_project_automation/case/test_login.py
+++ Operation_project_automation/case/test_login.py
@@ -10,22 +10,27 @@ class LoginCase(unittest.TestCase):
         cls.driver = webdriver.Firefox()
         cls.loginpage = LoginPage(cls.driver)
 
-    def login_case(self, username, psw):
-        """lonin"""
-        pass
-        # self.loginpage.login(username, psw)
-        # result = self.loginpage.get_login_result()
-        # return result
+    # def login_case(self, username, psw):
+    #     """lonin"""
+    #     pass
+    #     # self.loginpage.login(username, psw)
+    #     # result = self.loginpage.get_login_result()
+    #     # return result
 
-    def test_login1(self, username='cyf-02', psw='Abc123456.'):  #
+    def test_login1(self, username='cyf-01', psw='Abc123456.'):  #
         """SuccessCase"""
         self.loginpage.login(username, psw)
         result = self.loginpage.get_login_result()
         self.assertTrue(result == 'cyf-01')
 
-    def test_login2(self):
+    def test_login2(self, username='cyf-1245', psw='123456'):
         """failcase"""
-        pass
+        self.loginpage.login(username, psw)
+        result = self.loginpage.get_login_result()
+        self.assertTrue(result == 'cyf-01')
+
+    # def log_out(self):
+    #     self.loginpage.logout()
 
     @classmethod
     def tearDownClass(cls):
